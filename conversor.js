9efba4559e650def365de6eaa9508fdd87d802b5
PedroBarbosaSantana <pedrofbs@hotmail.com>
2023-10-24 23:29:25 -0300

    Conversor de unidade de medidas.

---
+++ conversor.js
@@ -0,0 +1,13 @@
+
+const measure = prompt("Insira uma medida em metros: ")
+
+const unityMeasurement = prompt("Qual unidade de medida deseja converter?" +
+"\n 1) Milímetros (MM)\n" +
+"\n 2) Centímetros (CM)\n" +
+"\n 3) Decímetros (DM)\n" +
+"\n 4) Decâmetro(DAM)\n" +
+"\n 5) Hectômetro (HM)\n" +
+"\n 6) Quilômetro (KM)\n"
+
+
+)
