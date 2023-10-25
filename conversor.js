a734808bb2f211210e780de330e2f17c18dd8983
PedroBarbosaSantana <pedrofbs@hotmail.com>
2023-10-24 23:34:31 -0300

    Conversor Unidade De Medidas

--- conversor.js
+++ conversor.js
@@ -9,5 +9,28 @@ const unityMeasurement = prompt("Qual unidade de medida deseja converter?" +
 "\n 5) Hectômetro (HM)\n" +
 "\n 6) Quilômetro (KM)\n"
 
-
 )
+
+switch(unityMeasurement) {
+    case "1":
+        alert("Resultado: " + measure + "m = " + measure * 1000 + "mm")
+        break
+    case "2": 
+        alert("Resultado: " + measure + "m = " + measure * 100 + "cm")
+        break
+    case "3":
+        alert("Resultado: " + measure + "m = " + measure * 10 + "dm")
+        break
+    case "4":
+        alert("Resultado: " + measure + "m = " + measure /10 + "dam")
+        break
+    case "5":
+        alert("Resultado: " + measure + "m = " + measure /100 + "hm")
+        break
+    case "6":
+        alert("Resultado: " + measure + "m = " + measure / 1000 + "km")
+        break
+    default:
+        alert("Opção inválida.")
+
+}
